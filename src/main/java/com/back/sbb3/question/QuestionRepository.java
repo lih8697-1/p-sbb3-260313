5738a20311270e0867cd609650e33ee353a8fff8
lih8697-1 <lih8697@gmail.com>
2026-03-15 23:58:42 +0900

    3-13

--- src/main/java/com/back/sbb3/question/QuestionRepository.java
+++ src/main/java/com/back/sbb3/question/QuestionRepository.java
@@ -2,6 +2,7 @@ package com.back.sbb3.question;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -14,4 +15,6 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     List<Question> findBySubjectLike(String subject);
 
     Page<Question> findAll(Pageable pageable);
+
+    Page<Question> findAll(Specification<Question> spec, Pageable pageable);
 }
