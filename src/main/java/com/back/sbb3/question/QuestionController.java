427b6d29b8c7636cf4709cbe74a5ee00941b6dec
lih8697-1 <lih8697@gmail.com>
2026-03-15 15:47:21 +0900

    2-16

--- src/main/java/com/back/sbb3/question/QuestionController.java
+++ src/main/java/com/back/sbb3/question/QuestionController.java
@@ -1,10 +1,14 @@
 package com.back.sbb3.question;
 
+import com.back.sbb3.answer.AnswerForm;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -24,11 +28,23 @@ public class QuestionController {
     }
 
     @GetMapping(value = "/detail/{id}")
-    private String detail(Model model, @PathVariable("id") Integer id) {
+    private String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
     }
 
+    @GetMapping("/create")
+    public String quetionCreate(QuestionForm questionForm) {
+        return "question_form";
+    }
 
+    @PostMapping("/create")
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/question/list"; // 질문 저장 후 질문 목록으로 이동
+    }
 }
