0a5b9c2c28a6b35334b9b50e18fc23739e8905d2
lih8697-1 <lih8697@gmail.com>
2026-03-15 16:08:33 +0900

    3-2

--- src/main/java/com/back/sbb3/question/QuestionService.java
+++ src/main/java/com/back/sbb3/question/QuestionService.java
@@ -2,9 +2,14 @@ package com.back.sbb3.question;
 
 import com.back.sbb3.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,6 +31,13 @@ public class QuestionService {
         else {
             throw new DataNotFoundException("question not found");
         }
+    }
+
+    public Page<Question> getList(int page){
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
     }
 
     public void create(String subject, String content) {
